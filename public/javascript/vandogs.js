949ef314b54cfb2f9ed10993483fd175c15c6db1
samanthadoesstuff <52324336+samanthadoesstuff@users.noreply.github.com>
2020-03-23 16:57:46 -0500

    update contact us page with js

--- public/javascript/vandogs.js
+++ public/javascript/vandogs.js
@@ -1,11 +1,22 @@
 // for contact form
 
-function logSubmit(event) {
-    log.textContent = `Form Submitted! Thank you!`;
-    event.preventDefault();
-  }
+// function logSubmit(event) {
+//     log.textContent = `Form Submitted! Thank you!`;
+//     event.preventDefault();
+//   }
   
-  const form = document.getElementById('form');
-  const log = document.getElementById('log');
-  form.addEventListener('submit', logSubmit);
+//   const form = document.getElementById('form');
+//   const log = document.getElementById('log');
+//   form.addEventListener('submit', logSubmit);
   
+const submitBtn = document.getElementById("btn");
+const submitMessage = document.getElementById("submitText");
+
+function submit() {
+  submitMessage.textContent = "Thanks! We'll get back to you as soon as we can.";
+};
+
+submitBtn.addEventListener("click", function(event){
+  event.preventDefault();
+  submit();
+});
